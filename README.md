bfdaddb9230f7975bc3af62685761ae0a1ad67c5
Gabriel Moura <gabrielpratamg.gm@gmail.com>
2024-12-24 20:02:07 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,18 @@
+<h1>🎮 Jogo do número secreto 🎮 </h1>
+
+<h2> Sobre </h2>
+<p>Projeto utilizado nos cursos de lógica de programação da Alura.</p>
+
+<h3> 💻 Linguagens 💻 </h3> 
+<div>
+  <img src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white">
+  <img src="https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white">
+  <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+</div>
+<h4> Responsável </h4> 
+<div style="border: 2px solid black; padding: 10px; border-radius: 10px; width: 130px; text-align: center;">
+  <a href="https://github.com/Halwyr" style="text-decoration: none; color: inherit;">
+    <img src="https://avatars.githubusercontent.com/u/185221744?v=4" width="115" height="115" style="border-radius: 10%;"><br>
+    <sub style="color: red; font-weight: bold; margin-top: 5px; display: block;">Gabriel Moura</sub>
+  </a>
+</div>
---
+++ app.js
@@ -0,0 +1,120 @@
+// Lista para não repetir números sorteados.
+let listaDeNumerosSorteados = [];
+
+// Declara o número limite do jogo.
+let numeroLimite = 100;
+
+// Variável para armazenar o número aleatório.
+let numeroSecreto =  gerarNumeroAleatorio();
+
+// Variável para armazenar o número de tentativas do usuário.
+let tentativas = 1;
+
+// Função para exibir os textos através de tags.
+function exibirTextoNaTela(tag, texto) {
+
+        let campo = document.querySelector(tag);
+        campo.innerHTML = texto; 
+        if ('speechSynthesis' in window) {
+                let utterance = new SpeechSynthesisUtterance(texto);
+                utterance.lang = 'pt-BR'; 
+                utterance.rate = 1.2; 
+                window.speechSynthesis.speak(utterance); 
+            } else {
+                console.log("Web Speech API não suportada neste navegador.");
+            }
+};
+
+//Função para reiniciar o jogo com as mensagens.
+function exibirMensagemInicial() {
+
+        exibirTextoNaTela('h1', 'Jogo do Número Secreto.');
+        exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}.` );
+
+};
+
+exibirMensagemInicial();
+
+
+// Redução de linhas de código ao criar funções.
+exibirTextoNaTela('h1', 'Jogo do Número Secreto.');
+exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}.`);
+
+// Função para clique do botão.
+function verificarChute() {
+
+        let chute = document.querySelector('input').value;
+
+        if ( chute == numeroSecreto) {
+
+                exibirTextoNaTela('h1', 'Acertou!');
+
+                let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+
+                let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}.`;
+
+                exibirTextoNaTela('p', mensagemTentativas);
+
+                document.getElementById('reiniciar').removeAttribute('disabled');
+
+        } else {
+                if(chute > numeroSecreto) {
+
+                        exibirTextoNaTela('p', `O número secreto é menor que ${chute}!`);
+
+                } else {
+
+                        exibirTextoNaTela('p', `O número secreto é maior que ${chute}!`);
+
+                }
+
+                tentativas++;
+
+                limparCampo();
+        };
+
+};
+
+// Função para limpar o campo após o chute.
+function limparCampo() {
+
+        chute = document.querySelector('input');
+        chute.value = '';
+
+};
+
+// Função para reiniciar o jogo após acertar.
+function reiniciarJogo() {
+
+        numeroSecreto = gerarNumeroAleatorio();
+        limparCampo();
+        tentativas = 1;
+        exibirMensagemInicial();
+        document.getElementById('reiniciar').setAttribute('disabled' , true);
+
+};
+
+// Criada a função para gerar um número aleatório e adicionada na lista.
+function gerarNumeroAleatorio() {
+
+       let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+       let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+       if (quantidadeDeElementosNaLista == numeroLimite) {
+
+                listaDeNumerosSorteados = [];
+       };
+        
+       if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+
+                return gerarNumeroAleatorio();
+
+       } else {
+
+                listaDeNumerosSorteados.push(numeroEscolhido);
+                console.log(listaDeNumerosSorteados);
+                return numeroEscolhido;
+
+       };
+
+}
